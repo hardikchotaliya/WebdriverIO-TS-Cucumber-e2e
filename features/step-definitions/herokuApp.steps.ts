2d66d7e2bccabd9058c7cba0ec6d575a92a2cb13
Hardik Chotaliya <14141734+hardikchotaliya@users.noreply.github.com>
2024-02-16 21:04:45 +0530

    Added Broken image TC

--- features/step-definitions/herokuApp.steps.ts
+++ features/step-definitions/herokuApp.steps.ts
@@ -2,36 +2,36 @@ import { Given, When, Then } from '@wdio/cucumber-framework';
 import HerokuAppPage from '../pageobjects/herokuApp.page.js';
 import herokuAppPage from '../pageobjects/herokuApp.page.js';
 
-Given(/^I am on the Herokuapp ([^"]*) page with ([^"]*) path$/, async (pageName, path?) => {
+Given(/^I am on the Herokuapp ([^"]*) page with ([^"]*) path$/, async function (pageName, path?) {
     // reporter.addStep(this.testid, "info", "Verifying forget password message...")
-    await HerokuAppPage.openHerokuapp(path);
+    await HerokuAppPage.openHerokuapp(this.testid, path);
 });
 
-Given(/^I am on the "([^"]*)" page with "([^"]*)" URL$/, async (pageName, url) => {
+Given(/^I am on the "([^"]*)" page with "([^"]*)" URL$/, async function (pageName, url) {
     await HerokuAppPage.openWebsite(url);
 });
 
-When(/^I verify the Herokuapp homepage$/, async () => {
-    await HerokuAppPage.verifyHomepageHeading();
+When(/^I verify the Herokuapp homepage$/, async function () {
+    await HerokuAppPage.verifyHomepageHeading(this.testid);
 });
 
 When(/^I click on ([^"]*) link$/, async (link) => {
     await HerokuAppPage.clickLink(link);
 });
 
-Then(/^I should navigate to the ([^"]*) page with heading - ([^"]*)$/, async (url, heading) => {
+Then(/^I should navigate to the ([^"]*) page with heading - ([^"]*)$/, async function (url, heading) {
     await HerokuAppPage.checkURLContains(url);
-    await HerokuAppPage.verifyPageHeading(heading);
+    await HerokuAppPage.verifyPageHeading(this.testid, heading);
 });
 
-When(/^I verify the "([^"]*)" Element functionality$/, async (action) => {
-    await herokuAppPage.verifyAddRemoveElementFunctionality(action);
+When(/^I verify the "([^"]*)" Element functionality$/, async function (action) {
+    await herokuAppPage.verifyAddRemoveElementFunctionality(this.testid, action);
 });
 
-Then(/^I verify the body message "([^"]*)" of the page$/, async (expectedMsg) => {
-    await HerokuAppPage.checkBodyMessage(expectedMsg);
+Then(/^I verify the body message "([^"]*)" of the page$/, async function (expectedMsg) {
+    await HerokuAppPage.checkBodyMessage(this.testid, expectedMsg);
 });
 
-Then(/^I check for the Broken Images on the page$/, async () => {
-    await HerokuAppPage.checkBrokenImages();
+Then(/^I check for the Broken Images on the page$/, async function () {
+    await HerokuAppPage.checkBrokenImages(this.testid);
 });
